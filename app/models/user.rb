6c3ad6433dde4a9e34fa85df7bc22132e4bbe942
Piotr Leniec <leniec.piotr@gmail.com>
2018-12-04 23:11:09 +0100

    add avatar field to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_one_attached :avatar
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :age, presence: true, numericality: { only_integer: true, greater_than: 0 }
